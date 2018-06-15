f8cf339a1522d0c6d02d8f865b0530323d4a17ee
Jannik Frauendorf <jannik.frauendorf@gmail.com>
2018-06-15 11:21:57 +0200

    Added function to read images

---
+++ image.py
@@ -0,0 +1,9 @@
+import numpy
+import cv2
+
+
+class Image:
+    def __init__(self):
+        self.classification = ""
+        self.image = numpy.empty
+        self.path = ""
--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-print("Hello World!")
-print("This is the begin of a perfect MMDBS!")
+import json
+
+import cv2
+import os
+import importer
+
+working_dir = os.path.dirname(os.path.realpath(__file__))
+path_source = working_dir + "/source/"
+
+images = importer.import_pictures(path_source)
+print(len(images))
+
+export_file_json = open(os.path.join(working_dir, "mmdbs.json"), "wt")
+
+
+export_data = []
+for image in images:
+    image_data = {"classification": image.classification, "path": image.path}
+    export_data.append(image_data)
+
+
+export_file_json.write(json.dumps(export_data))
+export_file_json.close()  # Close the file
