c2374f449822fbbd7f5c7aa063622b09fd5b3bf9
Jannik Frauendorf <jannik.frauendorf@gmail.com>
2018-06-15 11:48:04 +0200

    Initial commit

---
+++ mmdbs_images.sql
@@ -0,0 +1,13 @@
+-- auto-generated definition
+create table features
+(
+  id                    serial       not null
+    constraint features_pkey
+    primary key,
+  path                  varchar(255) not null,
+  classification        varchar(255),
+  local_histogram       json,
+  global_histogram      json,
+  global_edge_histogram json
+);
+
